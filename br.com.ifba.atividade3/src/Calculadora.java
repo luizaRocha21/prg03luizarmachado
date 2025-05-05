9ae10a1d72148b9c4b482d66498062946fce6973
20221ireads0006@ifba.edu.br <20221ireads0006@ifba.edu.br>
2025-05-04 23:35:00 -0300

    Merge origin/main

---
+++ br.com.ifba.atividade3/src/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author luiza
+ */
+public class Calculadora {
+    // Método que calcula a idade baseado no ano de nascimento
+    public static int calcularIdade(int anoNascimento) {
+        int anoAtual = 2025; // Ano fixo conforme seu requisito
+        return anoAtual - anoNascimento;
+    }
+    /**
+     * @param args the command line arguments
+     */
+}
